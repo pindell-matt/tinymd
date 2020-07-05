21285065fe6d55806df8b7bfabacf315764dad58
pindell-matt <pindell.m@protonmail.com>
2020-07-05 14:39:51 -0400

    add print_short_banner, delete usage

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-fn usage() {
-    println!("tinymd - a tiny markdown compiler")
+
+// tinymd (v0.1.0), a tiny and mostly useless markdown compiler. 
+// Written by <Your Name>
+// Usage: tinymd <somefile.md>
+
+fn print_short_banner() {
+    let title = String::from(env!("CARGO_PKG_NAME"));
+    let version = String::from(env!("CARGO_PKG_VERSION"));
+    let usage = format!("Usage: {title} <somefile.md>", title = title);
+
+    let banner = format!(
+        "{title} (v{version}), {description} \n{usage}",
+        title = title,
+        version = version,
+        description = "a tiny and mostly useless markdown compiler",
+        usage = usage
+    );
+
+    print!("{}", banner);
 }
 
 fn main() {
-    println!("Hello, world!");
+    print_short_banner()
 }
